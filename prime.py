05c8d467089a70c58133d911c5c586afe40f7b4e
MatthewWinsor <113452338+MatthewWinsor@users.noreply.github.com>
2022-10-06 11:04:57 -0400

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,3 @@
+import prime_Factors
+def generate_prime_factors():
+    prime_Factors.number_prime_or_not(6)
---
+++ prime_Factors.py
@@ -0,0 +1,18 @@
+
+PRIME_NUMBERS = []
+def number_prime_or_not(number):
+    try:
+        int(number)
+    except ValueError:
+        print("That's not a number sorry")
+
+
+    if number > 1:
+        for numbers in range(pyth2, number):
+            if (number % numbers) == 0:
+                PRIME_NUMBERS.append(numbers)
+            else:
+                pass
+    else:
+        return PRIME_NUMBERS
+    return PRIME_NUMBERS
